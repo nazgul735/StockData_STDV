cc1423c38c464ce9554d0e48efe6e9f1726373a0
nazgul735 <68790240+nazgul735@users.noreply.github.com>
2022-03-14 17:53:56 +0100

    played with the pandas lib

---
+++ StandardDeviation.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def get_dataframe_from_csv(filename):
+    df = pd.read_csv(filename)
+    return df
+
+
+stock = "BABA"
+df = get_dataframe_from_csv('{}.csv'.format(stock))
+
+data = df['Close']
+mean = df['Close'].mean()
+std = df['Close'].std()
+min_value = min(data)
+max_value = max(data)
+
+plt.title("BABA Dataset")
+plt.ylim(min_value - 100, max_value + 100)
+plt.scatter(x=df.index, y=df['Close'])
+plt.hlines(y=mean, xmin=0, xmax=len(data))
+plt.show()
