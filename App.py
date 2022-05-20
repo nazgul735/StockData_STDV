0e15c56c8deecc333ae95f4cdee64eb7271b90a7
Nazgul735 <sebastian.veum@gmail.com>
2022-05-20 13:00:47 +0200

    feat: added figlet

--- App.py
+++ App.py
@@ -1,4 +1,9 @@
 from Commands import Commands as cmd
+from pyfiglet import Figlet
+
+f = Figlet(font="slant")
+print(f.renderText("AlgoQuant"))
+    
 print("Add tickers")
 cmd.addTickers()
 print("To see the menu, press help (h)")
@@ -6,7 +11,7 @@ while (ui := input("> ").lower()) != "q":
     cmd.help() if (ui == ("help")) or (ui==("h")) else next
     cmd.getStdv() if (ui == ("stdv")) else next
     cmd.getMeanPrice() if (ui == ("mean")) else next
-    cmd.getCurrentPrice() if (ui == ("currprc")) else next
+    cmd.getCurrentPrice() if (ui == ("curr")) else next
     cmd.getPriceToMean() if (ui == ("p/m")) else next
     cmd.getStdvToPriceRatio() if (ui == ("stdv/curr")) else next
 
