b4161cd0d620a0a22ba26eb8f18f2e612483dae0
Nazgul735 <sebastian.veum@gmail.com>
2022-05-24 20:45:52 +0200

    fix: improved create portfolio

--- CreatePortfolio.py
+++ CreatePortfolio.py
@@ -3,19 +3,16 @@ import yfinance as yf
 class CreatePortfolio:
     def createPortfolio(portfolio, period):
         finalPortfolio={}
-        try:
-            for tckr in portfolio:
-                tckr=tckr.upper()
-                try:
-                    tempTicker = yf.Ticker(tckr)
-                    tempData = tempTicker.history(period)
-                    tempClosingPrice= tempData["Close"]
-                    finalPortfolio[tckr]=[round(val, 2) for val in tempClosingPrice.tolist()]
-                except:
-                    print(f"Couldn't find data for {tckr}")
-                    continue
-        except Exception as e:
-            print("Failed to get required data.", e)   
+        for tckr in portfolio:
+            tckr=tckr.upper()
+            try:
+                tempTicker = yf.Ticker(tckr)
+                tempData = tempTicker.history(period)
+                tempClosingPrice= tempData["Close"]
+                finalPortfolio[tckr]=[round(val, 2) for val in tempClosingPrice.tolist()]
+            except:
+                print(f"Couldn't find data for {tckr}")
+                continue 
         return finalPortfolio
 
 
